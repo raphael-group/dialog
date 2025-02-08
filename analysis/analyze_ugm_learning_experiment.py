d86bec386d2beedc82fcc9954d4de7dc1fd12acd
ashuaibi7 <ashuaibi@princeton.edu>
2025-02-08 17:39:06 -0500

    feature: implement variational inference model
    - Save table of observed and expected mutation counts to use as naive
      point of reference
    - Implement basic variational inference model that uses count data and
      BMR distributions to learn theta and beta parameters

--- analysis/analyze_ugm_learning_experiment.py
+++ analysis/analyze_ugm_learning_experiment.py
@@ -6,11 +6,70 @@ from pathlib import Path
 import argcomplete
 import numpy as np
 import pandas as pd
+import torch
+from torch import optim
 
 from models.binarized_count_ising import fit_binarized_count_data_ising_model
 from models.bmr_aware_binarized_count_ising import (
     fit_bmr_aware_binarized_count_data_ising_model,
 )
+
+
+# ------------------------------------------------------------------------------------ #
+#                                        MODELS                                        #
+# ------------------------------------------------------------------------------------ #
+def run_mean_field_variational_inference(
+    count_data: np.ndarray,
+    gene_to_bmr_pmf: dict,
+    num_iterations: int = 50,
+    lr: float = 1e-2,
+) -> tuple:
+    """TODO: Add docstring."""
+    n_samples, p = count_data.shape
+    m_param = torch.zeros(n_samples, p, requires_grad=True)
+    theta_param = torch.zeros(p, requires_grad=True)
+    beta_param = torch.zeros(p, p, requires_grad=True)
+    optimizer = optim.Adam([m_param, theta_param, beta_param], lr=lr)
+
+    def log_prob_counts(x: float, mean_spin: float, bmr_probs: torch.tensor) -> float:
+        return -((1 + mean_spin) / 2.0 * (x - bmr_probs.mean()) ** 2).sum()
+
+    def free_energy() -> float:
+        m = torch.tanh(m_param)
+        energy = 0.0
+        energy += (m * theta_param).sum()
+        for n in range(n_samples):
+            mn = m[n]
+            energy += torch.triu(beta_param, diagonal=1).mul(torch.ger(mn, mn)).sum()
+        entropy = 0.0
+        eps = 1e-7
+        m_clamped = torch.clamp(m, -1 + eps, 1 - eps)
+        p_plus = 0.5 * (1.0 + m_clamped)
+        p_minus = 0.5 * (1.0 - m_clamped)
+        entropy -= (p_plus * torch.log(p_plus) + p_minus * torch.log(p_minus)).sum()
+        count_likelihood = 0.0
+        for i in range(p):
+            bmr_probs = torch.tensor(gene_to_bmr_pmf.get(i)).float()
+            for n in range(n_samples):
+                count_likelihood += log_prob_counts(
+                    count_data[n, i],
+                    m[n, i],
+                    bmr_probs,
+                )
+        return -(energy + entropy + count_likelihood)
+
+    for _i in range(num_iterations):
+        optimizer.zero_grad()
+        f = free_energy()
+        f.backward()
+        optimizer.step()
+
+    with torch.no_grad():
+        m_final = torch.tanh(m_param).detach().cpu().numpy()
+        theta_final = theta_param.detach().cpu().numpy()
+        beta_final = 0.5 * (beta_param + beta_param.T)
+        beta_final = beta_final.detach().cpu().numpy()
+    return m_final, theta_final, beta_final
 
 
 # ------------------------------------------------------------------------------------ #
@@ -26,10 +85,11 @@ def _build_argument_parser() -> ArgumentParser:
     return parser
 
 
-def _calculate_expected_per_sample_background_mutations(
+def _calculate_expected_background_mutations(
+    num_samples: int,
     bmr_pmf_vals: list,
 ) -> float:
-    return np.sum([x * prob_x for x, prob_x in enumerate(bmr_pmf_vals)])
+    return num_samples * np.sum([x * prob_x for x, prob_x in enumerate(bmr_pmf_vals)])
 
 
 def _calculate_prob_nonzero_background_mutations(
@@ -99,6 +159,35 @@ def run_bmr_aware_binarized_count_data_ising_model(
     df_pairwise.to_csv(out_dir / "bmr_ising_pairwise_beta_values.csv", index=False)
 
 
+def create_obs_exp_mutation_table(
+    gene_to_bmr_pmf: dict,
+    cnt_mtx_df: pd.DataFrame,
+) -> pd.DataFrame:
+    """TODO: Add docstring."""
+    genes = cnt_mtx_df.columns
+    expected_mutations = {
+        gene: _calculate_expected_background_mutations(
+            cnt_mtx_df.shape[0],
+            gene_to_bmr_pmf[gene],
+        )
+        for gene in genes
+    }
+    observed_mutations = cnt_mtx_df.sum().to_dict()
+
+    result_df = pd.DataFrame(
+        {
+            "gene": genes,
+            "observed": [observed_mutations[gene] for gene in genes],
+            "expected": [expected_mutations[gene] for gene in genes],
+            "obs_minus_exp": [
+                observed_mutations[gene] - expected_mutations[gene] for gene in genes
+            ],
+        },
+    )
+
+    return result_df.sort_values(by="obs_minus_exp", ascending=False)
+
+
 # ------------------------------------------------------------------------------------ #
 #                                     MAIN FUNCTION                                    #
 # ------------------------------------------------------------------------------------ #
@@ -120,29 +209,30 @@ def main() -> None:
         k: [val for val in pmf_vals if not np.isnan(val)]
         for k, pmf_vals in gene_to_bmr_pmf_raw.items()
     }
-    gene_to_exp_sample_bkgd_mutations = {
-        gene: _calculate_expected_per_sample_background_mutations(
-            bmr_pmf_vals,
-        )
-        for gene, bmr_pmf_vals in gene_to_bmr_pmf.items()
-    }
-    gene_to_exp_sample_somatic_mutations = {
-        gene: sub_cnt_mtx_df[gene].mean() for gene in genes
-    }
+    # create observed vs. expected mutation table
+    obs_exp_mut_table = create_obs_exp_mutation_table(gene_to_bmr_pmf, sub_cnt_mtx_df)
+    obs_exp_mut_table.to_csv(args.out_dir / "obs_exp_mutation_table.csv", index=False)
 
-    run_sparse_logistic_regression_ising_model(
-        (sub_cnt_mtx_df.to_numpy() > 0).astype(int),
-        genes,
-        args.out_dir,
+    # run mean field variational inference
+    arr_counts = sub_cnt_mtx_df.to_numpy()
+    m_final, theta_final, beta_final = run_mean_field_variational_inference(
+        arr_counts,
+        {i: gene_to_bmr_pmf.get(g) for i, g in enumerate(genes)},
     )
-
-    run_bmr_aware_binarized_count_data_ising_model(
-        sub_cnt_mtx_df.to_numpy(),
-        genes,
-        gene_to_exp_sample_bkgd_mutations,
-        gene_to_exp_sample_somatic_mutations,
-        args.out_dir,
-    )
+    df_theta = pd.DataFrame(
+        zip(genes, theta_final),
+        columns=["gene", "theta"],
+    ).sort_values(by="theta", ascending=True)
+    df_theta.to_csv(args.out_dir / "mf_single_theta_values.csv", index=False)
+    df_beta = pd.DataFrame(
+        [
+            (gene_a, gene_b, beta_final[i, j])
+            for i, gene_a in enumerate(genes)
+            for j, gene_b in enumerate(genes[i + 1 :], start=i + 1)
+        ],
+        columns=["gene_a", "gene_b", "beta_ij"],
+    ).sort_values(by="beta_ij", ascending=True)
+    df_beta.to_csv(args.out_dir / "mf_pairwise_beta_values.csv", index=False)
 
 
 if __name__ == "__main__":
