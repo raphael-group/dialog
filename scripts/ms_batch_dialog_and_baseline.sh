db7a99ebf74301eb02efe17bf878cba2ed13fd86
ashuaibi7 <ashuaibi@princeton.edu>
2025-03-27 14:17:55 -0400

    fix: update batch script to run ME and CO

---
+++ scripts/ms_batch_dialog_and_baseline.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir -p output/slurm
+
+for lr in $(seq 0.05 0.01 0.15); do
+  for m in $(seq 0.75 0.01 0.85); do
+    for ns in 1 3 5 10; do
+      for lambda in 0.00001 0.0001 0.001; do
+        JOB_NAME="dialog_lr${lr}_m${m}_ns${ns}_lambda${lambda}"
+        LOGFILE="output/slurm/${JOB_NAME}.out"
+        echo "Submitting ${JOB_NAME}"
+        sbatch --job-name="${JOB_NAME}" --output="${LOGFILE}" scripts/ss_batch_dialog_and_baseline.sh "${lr}" "${m}" "${ns}" "${lambda}"
+        sleep 0.20
+      done
+    done
+  done
+done
---
+++ scripts/ss_batch_dialog_and_baseline.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=10
+#SBATCH -t 04:00:00
+#SBATCH --mem=8GB
+#SBATCH --job-name=dialog
+
+# Get parameters from command-line arguments
+LR="$1"
+M="$2"
+S="$3"
+LAMBDA="$3"
+ME_SIM_NAME=NS5K_TL0.15_TH0.25_25ME_0CO_150LP
+CO_SIM_NAME=NS5K_TL0.02_TH0.06_0ME_25CO_150LP
+
+for SIM_NAME in "$ME_SIM_NAME" "$CO_SIM_NAME"; do
+  nice python src/dialog/__main__.py \
+    -c data/simulations/UCEC/${SIM_NAME}/count_matrix.csv \
+    -b data/simulations/UCEC/${SIM_NAME}/bmr_pmfs.csv \
+    -o output/${SIM_NAME}/ \
+    -lr "${LR}" -m "${M}" -s "${S}" -lt "${LAMBDA}" -lb "${LAMBDA}"
+
+  nice python src/baselines/__main__.py \
+    -c data/simulations/UCEC/${SIM_NAME}/count_matrix.csv \
+    -o output/${SIM_NAME}/
+done
